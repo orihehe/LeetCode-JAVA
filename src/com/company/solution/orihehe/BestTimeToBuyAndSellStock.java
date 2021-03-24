1f524bddd48153d97aae304486a5b5e894b893d1
orihehe <38060133+orihehe@users.noreply.github.com>
2021-03-24 23:23:12 +0900

    Add solution of 121

---
+++ src/com/company/solution/orihehe/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,28 @@
+package com.company.solution.orihehe;
+
+import java.util.Arrays;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        final int sz = prices.length;
+
+        int[] mn = new int[sz];
+        int[] mx = new int[sz];
+
+        Arrays.fill(mn, 10001);
+
+        int ans = 0;
+        mn[0] = prices[0];
+        for (int i = 1; i < sz; i++) {
+            mn[i] = Math.min(mn[i - 1], prices[i]);
+        }
+        mx[sz - 1] = prices[sz - 1];
+        for (int i = sz - 2; i >= 0; i--) {
+            mx[i] = Math.max(mx[i + 1], prices[i]);
+        }
+        for (int i = 0; i < sz - 1; i++) {
+            ans = Math.max(ans, mx[i + 1] - mn[i]);
+        }
+        return ans;
+    }
+}
