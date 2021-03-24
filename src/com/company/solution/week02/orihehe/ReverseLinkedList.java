6a459c0e737e6b8144efd320e9d016dfa95496da
orihehe <38060133+orihehe@users.noreply.github.com>
2021-03-25 00:10:36 +0900

    Add solution of 206

---
+++ src/com/company/solution/week02/orihehe/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package com.company.solution.week02.orihehe;
+
+
+import com.company.solution.week02.common.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 206
+// O(N)
+public class ReverseLinkedList {
+
+    private void solve(List<Integer> l, ListNode head) {
+        if (head == null) return;
+        solve(l, head.next);
+        l.add(head.val);
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+
+        List<Integer> values = new ArrayList<>();
+        solve(values, head);
+
+        ListNode ansHead = new ListNode(values.get(0));
+        ListNode cur = ansHead;
+        for (int i=1; i<values.size(); i++) {
+            cur.next = new ListNode(values.get(i), null);
+            cur = cur.next;
+        }
+
+        return ansHead;
+    }
+}
