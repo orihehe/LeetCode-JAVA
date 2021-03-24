f03d2476f9cf22804112148cc09649f7e93f8f3c
BJuyoung <qkdwn0313@gmail.com>
2021-03-24 23:18:41 +0900

    weekly_1

---
+++ src/com/company/solution/bbahngju/BestTimetoBuyandSellStock.java
@@ -0,0 +1,24 @@
+package com.company.solution.bbahngju;
+
+public class BestTimetoBuyandSellStock {
+    public static void main(String[] arg) {
+        int[] prices = new int[]{7,6,4,3,1};
+        int result = maxProfit(prices);
+        System.out.println(result);
+    }
+    public static int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int maximum = 0;
+        for(int i=1; i<prices.length; i++) {
+            if(buy > prices[i]) {
+                buy = prices[i];
+                continue;
+            }
+            else{
+                maximum = Math.max(maximum,prices[i]-buy);
+            }
+        }
+
+        return maximum;
+    }
+}
---
+++ src/com/company/solution/bbahngju/VerifyingAnAlienDictionary.java
@@ -0,0 +1,40 @@
+package com.company.solution.bbahngju;
+
+import java.util.Arrays;
+
+public class VerifyingAnAlienDictionary {
+    public static void main(String[] arg) {
+        String[] a = new String[]{"fxasxpc","dfbdrifhp","nwzgs","cmwqriv","ebulyfyve","miracx","sxckdwzv","dtijzluhts","wwbmnge","qmjwymmyox"};
+        String input = "zkgwaverfimqxbnctdplsjyohu";
+        System.out.println(isAlienSorted(a, input));
+    }
+
+    public static boolean isAlienSorted(String[] words, String order) {
+        int[] alpha = new int[26];
+        for(int i=0; i<26; i++) alpha[i] = 0;
+
+        char[] dic = order.toCharArray();
+        for(int i=1; i<=dic.length; i++) {
+            alpha[dic[i-1]-'a'] = i;
+        }
+        System.out.println(Arrays.toString(alpha));
+        for(int i=0; i<words.length-1; i++) {
+            int minimum = Math.min(words[i].length(), words[i+1].length());
+            boolean check = false;
+            for(int j=0; j<minimum; j++) {
+                char first = words[i].charAt(j);
+                char second = words[i+1].charAt(j);
+                if(first == second) continue;
+                if(alpha[first-'a'] < alpha[second-'a']) {
+                    check = true;
+                    break;
+                }
+                if(alpha[first-'a'] > alpha[second-'a']) return false;
+            }
+            if(!check && words[i].length() > words[i+1].length())
+                return false;
+        }
+
+        return true;
+    }
+}
