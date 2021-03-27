343fefef64a200a1aa1fd779d3b3f51ba5ab9a9c
BJuyoung <qkdwn0313@gmail.com>
2021-03-27 15:31:49 +0900

    Add 206

---
+++ src/com/company/solution/week02/bbahngju/ReverseLinkedList.java
@@ -0,0 +1,24 @@
+package com.company.solution.week02.bbahngju;
+
+import com.company.solution.week02.common.ListNode;
+
+//O(N)
+public class ReverseLinkedList {
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode preNode = head;
+            ListNode curNode = null;
+            ListNode nextNode = null;
+
+            while(preNode != null) {
+                nextNode = curNode;
+                curNode = preNode;
+                preNode = preNode.next;
+                curNode.next = nextNode;
+            }
+            head = curNode;
+
+            return head;
+        }
+    }
+}
