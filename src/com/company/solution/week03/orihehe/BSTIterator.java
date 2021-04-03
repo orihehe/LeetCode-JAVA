8a0af97cfcfb25fce68ef0a12b18a98c759d0f97
orihehe <38060133+orihehe@users.noreply.github.com>
2021-04-04 03:10:41 +0900

    Add solution of 173

---
+++ src/com/company/solution/week03/orihehe/BSTIterator.java
@@ -0,0 +1,33 @@
+package com.company.solution.week03.orihehe;
+
+import com.company.solution.week03.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 173
+// O(N)
+public class BSTIterator {
+
+    private List<Integer> arr = new ArrayList<>();
+    private int iter = 0;
+
+    private void init(TreeNode root) {
+        if (root == null) return;
+        init(root.left);
+        arr.add(root.val);
+        init(root.right);
+    }
+
+    public BSTIterator(TreeNode root) {
+        init(root);
+    }
+
+    public int next() {
+        return arr.get(iter++);
+    }
+
+    public boolean hasNext() {
+        return iter < arr.size();
+    }
+}
