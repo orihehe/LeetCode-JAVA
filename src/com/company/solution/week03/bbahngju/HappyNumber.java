be151af0b2268ac5fe3c41d3fcdbdfbac7ea9cbe
BJuyoung <qkdwn0313@gmail.com>
2021-04-03 12:46:19 +0900

    modify 202, 283

--- src/com/company/solution/week03/bbahngju/HappyNumber.java
+++ src/com/company/solution/week03/bbahngju/HappyNumber.java
@@ -4,32 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HappyNumber {
-    class Solution {
-        private Map<Integer, Boolean> hashMap = new HashMap<>();
-        public boolean isHappy(int n) {
-            int total;
-            boolean happy = false;
+    private Map<Integer, Boolean> hashMap = new HashMap<>();
+    public boolean isHappy(int n) {
+        int total;
+        boolean happy = false;
 
-            hashMap.put(n, true);
-            while(true) {
-                total = 0;
-                while(n != 0) {
-                    total += Math.pow(n%10, 2);
-                    n /= 10;
-                }
-                if(total == 1) return true;
-                if(isCycle(total)) return false;
-
-                n = total;
-
+        hashMap.put(n, true);
+        while(true) {
+            total = 0;
+            while(n != 0) {
+                total += Math.pow(n%10, 2);
+                n /= 10;
             }
-        }
+            if(total == 1) return true;
+            if(isCycle(total)) return false;
 
-        public boolean isCycle(int t) {
-            if(hashMap.containsKey(t)) return true;
+            n = total;
 
-            hashMap.put(t, true);
-            return false;
         }
+    }
+
+    public boolean isCycle(int t) {
+        if(hashMap.containsKey(t)) return true;
+
+        hashMap.put(t, true);
+        return false;
     }
 }
--- src/com/company/solution/week03/bbahngju/MoveZeroes.java
+++ src/com/company/solution/week03/bbahngju/MoveZeroes.java
@@ -2,19 +2,17 @@ package com.company.solution.week03.bbahngju;
 
 //O(N)
 public class MoveZeroes {
-    class Solution {
-        public void moveZeroes(int[] nums) {
-           int move = 0;
-           for(int n : nums) {
-               if(n != 0){
-                   nums[move++] = n;
-               }
+    public void moveZeroes(int[] nums) {
+       int move = 0;
+       for(int n : nums) {
+           if(n != 0){
+               nums[move++] = n;
            }
-           if(move != nums.length) {
-               for(int i=move; i<nums.length; i++) {
-                   nums[i] = 0;
-               }
+       }
+       if(move != nums.length) {
+           for(int i=move; i<nums.length; i++) {
+               nums[i] = 0;
            }
-        }
+       }
     }
 }
