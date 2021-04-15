9fc3188545f430dffa168afeb368af8177cd95c4
orihehe <38060133+orihehe@users.noreply.github.com>
2021-04-15 21:30:05 +0900

    Add common class

---
+++ src/com/company/solution/week5/common/ListNode.java
@@ -0,0 +1,11 @@
+package com.company.solution.week5.common;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode() {}
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
