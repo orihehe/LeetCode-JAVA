4c0f649f03ec564cc4f83bf9452cb1a442c62d47
orihehe <38060133+orihehe@users.noreply.github.com>
2021-05-01 02:17:30 +0900

    Add solution of 28

---
+++ src/com/company/week07/orihehe/ImplementStr.java
@@ -0,0 +1,10 @@
+package com.company.week07.orihehe;
+
+// 28
+// O(NM)
+public class ImplementStr {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
