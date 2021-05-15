3016a24ec1e06f67806db28c077b90139b0a3575
orihehe <38060133+orihehe@users.noreply.github.com>
2021-05-15 23:59:32 +0900

    Add solution of 46

--- src/com/company/solution/week09/orihehe/ExcelSheetColumnNumber.java
+++ src/com/company/solution/week09/orihehe/ExcelSheetColumnNumber.java
@@ -1,39 +1,10 @@
 package com.company.solution.week09.orihehe;
 
-import java.util.ArrayList;
-import java.util.List;
-
 // 171
 // O(N!)
 public class ExcelSheetColumnNumber {
 
-    private final List<List<Integer>> ans = new ArrayList<>();
-    private final List<Integer> tmp = new ArrayList<>();
+    public int titleToNumber(String columnTitle) {
 
-    private int n;
-    private boolean[] used;
-
-    public List<List<Integer>> permute(int[] nums) {
-        n = nums.length;
-        used = new boolean[n];
-        dfs(nums, 0);
-        return ans;
-    }
-
-    private void dfs(int[] nums, int cur) {
-        if (cur == n) {
-            ans.add(new ArrayList<>() {{
-                addAll(tmp);
-            }});
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (used[i]) continue;
-            used[i] = true;
-            tmp.add(nums[i]);
-            dfs(nums, cur + 1);
-            used[i] = false;
-            tmp.remove(cur);
-        }
     }
 }
---
+++ src/com/company/solution/week09/orihehe/Permutations.java
@@ -0,0 +1,39 @@
+package com.company.solution.week09.orihehe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 46
+// O(N!)
+public class Permutations {
+
+    private final List<List<Integer>> ans = new ArrayList<>();
+    private final List<Integer> tmp = new ArrayList<>();
+
+    private int n;
+    private boolean[] used;
+
+    public List<List<Integer>> permute(int[] nums) {
+        n = nums.length;
+        used = new boolean[n];
+        dfs(nums, 0);
+        return ans;
+    }
+
+    private void dfs(int[] nums, int cur) {
+        if (cur == n) {
+            ans.add(new ArrayList<>() {{
+                addAll(tmp);
+            }});
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            tmp.add(nums[i]);
+            dfs(nums, cur + 1);
+            used[i] = false;
+            tmp.remove(cur);
+        }
+    }
+}
