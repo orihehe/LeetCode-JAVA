064e8b6f20c694792675d5945c2739de0f3d7424
orihehe <38060133+orihehe@users.noreply.github.com>
2021-05-16 01:08:31 +0900

    Add solution of 171

--- src/com/company/solution/week09/orihehe/ExcelSheetColumnNumber.java
+++ src/com/company/solution/week09/orihehe/ExcelSheetColumnNumber.java
@@ -1,10 +1,16 @@
 package com.company.solution.week09.orihehe;
 
 // 171
-// O(N!)
+// O(N)
 public class ExcelSheetColumnNumber {
 
     public int titleToNumber(String columnTitle) {
-
+        int p = 1, ans = 0;
+        for (int i = columnTitle.length() - 1; i >= 0; i--) {
+            int alpha = columnTitle.charAt(i) - 'A' + 1;
+            ans += alpha * p;
+            p *= 26;
+        }
+        return ans;
     }
 }
