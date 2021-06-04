987226955ea703e3c16adecf836f50bb83ff7cb9
orihehe <38060133+orihehe@users.noreply.github.com>
2021-06-05 03:32:56 +0900

    Modify String to StringBuilder

--- src/com/company/solution/week12/orihehe/LetterCombinationsOfAPhoneNumber.java
+++ src/com/company/solution/week12/orihehe/LetterCombinationsOfAPhoneNumber.java
@@ -12,19 +12,20 @@ public class LetterCombinationsOfAPhoneNumber {
     public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
         if (digits.length() > 0) {
-            dfs(ans, digits, "", 0);
+            dfs(ans, digits, new StringBuilder(), 0);
         }
         return ans;
     }
 
-    private void dfs(List<String> ans, String digits, String prev, int index) {
+    private void dfs(List<String> ans, String digits, StringBuilder prev, int index) {
         if (index >= digits.length()) {
-            ans.add(prev);
+            ans.add(prev.toString());
             return;
         }
         int mp = digits.charAt(index) - '0';
         for (char m : mapping[mp].toCharArray()) {
-            dfs(ans, digits, prev + m, index + 1);
+            dfs(ans, digits, prev.append(m), index + 1);
+            prev.deleteCharAt(index);
         }
     }
 }
