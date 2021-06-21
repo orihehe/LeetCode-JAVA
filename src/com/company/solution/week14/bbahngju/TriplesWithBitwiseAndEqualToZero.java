081ef123eb1e6548ebad7541168f4680fdb07996
Juyoung Bahng <qkdwn0313@gmail.com>
2021-06-21 16:52:19 +0900

    Add solution of 982

--- src/com/company/solution/week14/bbahngju/TriplesWithBitwiseAndEqualToZero.java
+++ src/com/company/solution/week14/bbahngju/TriplesWithBitwiseAndEqualToZero.java
@@ -1,23 +1,27 @@
 package com.company.solution.week14.bbahngju;
 
 //982
+//O((N^2)+(2^16 * N)) = O(66536000)
 public class TriplesWithBitwiseAndEqualToZero {
-    private int answer = 0;
+    int answer = 0;
 
     public int countTriplets(int[] nums) {
-        permutation(0, (int)Math.pow(2,16)-1, nums);
+        int[] countTwoNum = new int[1 << 16];
+
+        for(int first : nums) {
+            for(int second : nums) {
+                countTwoNum[first & second] += 1;
+            }
+        }
+
+        for(int i = 0; i < countTwoNum.length; i++) {
+            for(int third : nums) {
+                if((i & third) == 0) {
+                    answer += countTwoNum[i];
+                }
+            }
+        }
+
         return answer;
-    }
-
-    public void permutation(int idx, int andBit, int[] nums) {
-        if(idx == 3) {
-            if(andBit == 0)
-                answer++;
-            return;
-        }
-
-        for (int num : nums) {
-            permutation(idx + 1, andBit & num, nums);
-        }
     }
 }
