5ac2ce98d561f7d91eb5d7ba28d40f750622f950
orihehe <38060133+orihehe@users.noreply.github.com>
2021-08-03 00:21:22 +0900

    Add solution of 67

---
+++ src/com/company/solution/week15/AddBinary.java
@@ -0,0 +1,43 @@
+package com.company.solution.week15;
+
+// 67
+// O(N)
+public class AddBinary {
+
+    private String fillZero(String origin, int size) {
+        StringBuilder result = new StringBuilder();
+        while (origin.length() + result.length() < size) {
+            result.append('0');
+        }
+        result.append(origin);
+        return result.toString();
+    }
+
+    private String addBinaryString(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int up = 0;
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int ac = a.charAt(i) - '0';
+            int bc = b.charAt(i) - '0';
+            char c = (char)('0' + (ac + bc + up) % 2);
+            result.append(c);
+
+            if (ac + bc + up >= 2) {
+                up = 1;
+            } else {
+                up = 0;
+            }
+        }
+        if (up == 1) {
+            result.append('1');
+        }
+        return result.reverse().toString();
+    }
+
+    public String addBinary(String a, String b) {
+        int maxLen = Math.max(a.length(), b.length());
+        a = fillZero(a, maxLen);
+        b = fillZero(b, maxLen);
+        return addBinaryString(a, b);
+    }
+}
